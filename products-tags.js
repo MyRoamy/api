67834b4b3773fff30b9869b6ac62827038752aae
MyRoamy <capitalized.services@gmail.com>
2025-09-21 21:05:35 -0400

    Create products-tags.js

---
+++ products-tags.js
@@ -0,0 +1,12 @@
+import { viator, setCors } from "./_viator";
+export default async function handler(req, res) {
+  if (req.method === "OPTIONS") { setCors(res); return res.status(204).end(); }
+  try {
+    setCors(res);
+    const { lang = "en-US" } = req.query;
+    const data = await viator("/products/tags", { language: lang });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(err.status || 500).json({ error: "Viator error", detail: err.detail || String(err) });
+  }
+}
